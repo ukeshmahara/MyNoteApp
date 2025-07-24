1ff6109c68d205738b4b7b325523c27518049e6b
ukeshmahara <ukeshmahara361@gmail.com>
2025-07-24 20:02:29 +0545

    Adding LoginUiState

---
+++ app/src/main/java/com/ukesh/mynoteapp/data/models/ui_states/LoginUiState.kt
@@ -0,0 +1,12 @@
+package com.ukesh.mynoteapp.data.models.ui_states
+
+data class LoginUiState(
+    val userName:String = "",
+    val userEmail:String = "",
+    val userPassword:String = "",
+    val confirmPassword:String = "",
+    val isLoading:Boolean = false,
+    val isSuccess:Boolean = false,
+    val signUpError:String? = null,
+    val signInError:String? = null,
+)
