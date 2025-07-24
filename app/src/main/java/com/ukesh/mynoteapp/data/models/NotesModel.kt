c1df576b6d6c89c6f925e453f01984e0adaa707b
ukeshmahara <ukeshmahara361@gmail.com>
2025-07-24 20:04:57 +0545

    Adding NotesModel

---
+++ app/src/main/java/com/ukesh/mynoteapp/data/models/NotesModel.kt
@@ -0,0 +1,12 @@
+package com.ukesh.mynoteapp.data.models
+
+import com.google.firebase.Timestamp
+
+data class NotesModel(
+    val userId: String = "",
+    val documentId : String = "",
+    val noteTitle : String = "",
+    val notesDescription : String = "",
+    val timeStamp: Timestamp = Timestamp.now(),
+    val colorIndex: Int = 0
+)
