81fe312e22ed667156f117b3026d93d7174e4d8c
ukeshmahara <ukeshmahara361@gmail.com>
2025-07-25 20:53:09 +0545

    Graph.kt

---
+++ app/src/main/java/com/ukesh/mynoteapp/navigation/Graph.kt
@@ -0,0 +1,33 @@
+package com.ukesh.mynoteapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.ukesh.mynoteapp.navigation.mainNavGraph
+import com.ukesh.mynoteapp.presentation.screens.home.HomeViewModel
+
+object Graph {
+    const val ROOT_GRAPH = "root_graph"
+    const val AUTH_GRAPH = "auth_graph"
+    const val MAIN_GRAPH = "main_graph"
+}
+
+@Composable
+fun RootNavGraph(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel? = hiltViewModel()
+) {
+
+    val userExist = homeViewModel?.userExists ?: false
+
+    NavHost(
+        navController = navController,
+        startDestination = if (userExist) Graph.MAIN_GRAPH else Graph.AUTH_GRAPH,
+        route = Graph.ROOT_GRAPH
+    ) {
+        authNavGraph(navController)
+        mainNavGraph(navController)
+
+    }
+}
